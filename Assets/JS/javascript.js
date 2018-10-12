299c34103f9d4dd03d0909deee7282bbd153fa88
Boivin <boivinhelene@yahoo.com>
2018-10-12 13:42:59 +0200

    JS Partie 1

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,10 @@
+// var FirstName = prompt("Comment t'appelles-tu ?", "<Entrez ici votre prénom>");
+// alert("Bonjour " + FirstName);
+
+var FirstName = prompt("Comment t'appelles-tu ?", "<Entrez ici votre prénom>");
+if (isNaN(FirstName)){
+alert("Bonjour " + FirstName)
+}
+else {
+alert("Erreur!")
+}
